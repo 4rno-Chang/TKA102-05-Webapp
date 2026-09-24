6dd842865b0e01ec0d2aa8ea263f69c92aff685f
4rno-Chang <ckf1086691@gmail.com>
2026-09-24 17:15:46 +0800

    hibernate test

--- src/main/java/com/bistroops/announcement/model/AnnouncementDAO.java
+++ src/main/java/com/bistroops/announcement/model/AnnouncementDAO.java
@@ -14,7 +14,7 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import com.bistroops.util.HibernateUtil;
-
+ 
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.CriteriaBuilder;
 
